786888879a7923bd7bdcdc6d1e486e963e987cbc
albertgfliu <albertgfliu@gmail.com>
2016-01-12 21:58:49 -0800

    Update parsing function

--- main.c
+++ main.c
@@ -1,5 +1,70 @@
 //main.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 
-int main(int argc, char *argv[]){
-  return 1;
+//global variables
+int verbose_flag;
+
+int main(int argc, char **argv){
+
+	int curr_opt;
+	int curr_optind;
+	int next_opt;
+	int next_optind;
+	int argv_index;
+
+	static struct option long_opts[] = 
+	{
+		/*options that set a flag*/
+		{"verbose", no_argument, &verbose_flag, 1},
+
+		/*options that don't set a flag*/
+		{"rdonly",	required_argument,	0, 	'r'},
+		{"wronly",	required_argument,	0,	'w'},
+		{"command",	required_argument,	0,	'c'},
+		{"pipe",	no_argument,		0, 	'p'},
+		{0, 		0, 			0, 	0}
+	};
+	
+	curr_optind = 1;
+	next_opt = getopt_long(argc, argv, "", long_opts, NULL);
+	next_optind = optind;
+	while(next_opt != -1){
+		curr_opt = next_opt;
+
+		printf("curr_optind = %d, next_optind = %d\n", curr_optind, next_optind);
+		switch(curr_opt){
+			case 'r': //consume one file number
+				if(next_optind != curr_optind + 2)
+					printf("error: rdonly can only accept one argument\n"); //print to stderror?
+				else
+					printf("rdonly with arg %s\n", optarg);
+				break;
+			case 'w': //consume one file number
+				if(next_optind != curr_optind + 2)
+					printf("error: wronly can only accept one argument\n"); //print to stderror?
+				else
+					printf("rdonly with args %s\n", optarg);
+				break;
+				printf("wronly with arg %s\n", optarg);
+				break;
+			case 'p': //consumes two file numbers
+
+				if(next_optind != curr_optind + 1)
+					printf("error: pipe can not have any arguments");			
+				printf("pipe\n");
+				printf("pipe with arg = %s\n", optarg);	
+				break;
+			case 'c': //special case, will take more work
+				//parse multiple arguments
+				printf("command with args %s\n", optarg);
+				break;
+		}
+		
+		curr_optind = next_optind;
+		next_opt = getopt_long(argc, argv, "", long_opts, NULL);
+		next_optind = optind;
+	}
+  return EXIT_SUCCESS;
 }
--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #put test cases here
-./simpsh --rdonly a
-./simpsh --wronly b
-./simpsh --command
-./simpsh --verbose
-
-echo $?
+./simpsh --rdonly a --wronly b --pipe --rdonly c
+if [ $? -ne 0 ]; then
+	echo "Test 1 failed (temporary)"
+	exit
+else
+	echo "Test 1 success"
+fi
