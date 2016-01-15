6d79070c93d3fbd360db48c350843f67e409ca2e
albertgfliu <albertgfliu@gmail.com>
2016-01-15 12:11:39 -0800

    Change many things

--- main.c
+++ main.c
@@ -1,41 +1,55 @@
-// main.c
+//main.c
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/wait.h>
 
-// defines
+#include <unistd.h>
+
+//defines
 #define TRUE 1
 #define FALSE 0
+#define cmd_size 100
 
-// global variables
+//global variables
 static int verbose_flag;
-// logical to real file descriptors
+static int wait_flag;
 static int fd[1000];
+static int pipe_fd[2];
+static int cmd_fd[3];
+char *cmd[cmd_size];
+static int return_val;
+//char *cmd[20];
 
-// helper functions
-int isOption(char *cstring){
+//helper function
+int isAnOption(char *cstring){
 	if((cstring[0] != '-') || (cstring[1] != '-'))
 		return FALSE;
 	else
 		return TRUE;
 }
 
-// MUST PRINT ERROR MESSAGES FOR INVALID ARGUMENTS
+void flushCmd(){
+	for(int i = 0; i < cmd_size; i++)
+		cmd[i] = NULL;
+}
 
-// main function
+//MUST PRINT ERROR MESSAGES FOR INVALID ARGUMENTS
+
+//main function
 int main(int argc, char **argv){
 
 	static struct option long_opts[] = 
 	{
-		/* options that set a flag */
+		/*options that set a flag*/
 		{"verbose", no_argument, &verbose_flag, 1},
- 
-		/* options that don't set a flag */
+		{"wait", no_argument, &wait_flag, 1},
+
+		/*options that don't set a flag*/
 		{"rdonly",	required_argument,	0, 	'r'},
 		{"wronly",	required_argument,	0,	'w'},
 		{"command",	required_argument,	0,	'c'},
@@ -49,16 +63,25 @@ int main(int argc, char **argv){
 	int next_optind;
 
 	int fd_ind = 0;
+	int cmd_ind = 0;
 
+	verbose_flag = 0;
+	wait_flag = 0;
+
+	return_val = 0;
+
+	pid_t pid;
+	pid_t c_pid;
+	
 	curr_optind = optind;
 	curr_opt = getopt_long(argc, argv, "", long_opts, &long_opts_ind);
 	next_optind = optind;
 	if(curr_opt == -1){
-		fprintf(stderr, "Error: no options found\n");
+		fprintf(stderr, "error: no options found\n");
 	}
 	else {
-		if(!isOption(argv[curr_optind])){
-			fprintf(stderr, "Error: argument found before options and was ignored\n");
+		if(!isAnOption(argv[curr_optind])){
+			fprintf(stderr, "error: argument found before options and was ignored\n");
 		}
 		do {
 			//debug message
@@ -68,101 +91,136 @@ int main(int argc, char **argv){
 	
 			switch(curr_opt){
 				case 'r':{
-					if((next_optind != argc) && !isOption(argv[next_optind])){
-						fprintf(stderr, "Error: --rdonly can only accept one argument, further arguments to rdonly were ignored\n");
-					}
-					int temp_fd = open(optarg, O_RDONLY, 644);
-					if (temp_fd < 0){
-						fprintf(stderr, "Error: could not open file \"%s\" for read\n", optarg);
-                        exit(EXIT_FAILURE);
-					}
-					else{
-						fd[fd_ind++] = temp_fd;
+					if((next_optind != argc) && !isAnOption(argv[next_optind])){
+						fprintf(stderr, "error: rdonly can only accept one argument, further arguments to rdonly were ignored\n");
 					}
 					if(verbose_flag){
 						printf("--rdonly %s\n", optarg);
 					}
+					int temp_fd = open(optarg, O_RDONLY, 644);
+					if (temp_fd == -1){
+						fprintf(stderr, "error: could not open file \"%s\" for read\n", optarg);
+					}
+					fd[fd_ind++] = temp_fd;
+					
 					break;
 				}
 				case 'w':{
-					if((next_optind != argc) && !isOption(argv[next_optind])){
-						fprintf(stderr, "Error: wronly can only accept one argument, further arguments to wronly were ignored\n");
-					}
-					int temp_fd = open(optarg, O_WRONLY, 644);
-					if (temp_fd < 0){
-						fprintf(stderr, "Error: could not open file \"%s\" for write\n", optarg);
-                        exit(EXIT_FAILURE);
-					}
-					else{
-						fd[fd_ind++] = temp_fd;
+					if((next_optind != argc) && !isAnOption(argv[next_optind])){
+						fprintf(stderr, "error: wronly can only accept one argument, further arguments to wronly were ignored\n");
 					}
 					if(verbose_flag){
 						printf("--wronly %s\n", optarg);
 					}
+					int temp_fd = open(optarg, O_WRONLY, 644);
+					if (temp_fd == -1){
+						fprintf(stderr, "error: could not open file \"%s\" for write\n", optarg);
+					}
+					fd[fd_ind++] = temp_fd;
+					
+					break;
+				}
+				case 'p':{
+					if((next_optind != argc) && !isAnOption(argv[next_optind])){
+						fprintf(stderr, "error: pipe can not accept any arguments, all arguments to pipe were ignored\n");
+					}
+					if(verbose_flag){
+						printf("--pipe\n");
+					}
+					if(pipe(pipe_fd) == -1){
+						fprintf(stderr, "error: failure to create a pipe\n");
+					}
+					fd[fd_ind++] = pipe_fd[0];
+					fd[fd_ind++] = pipe_fd[1];
+
 					break;
 				}
 				case 'c':{
-                    pid_t pid, wpid;
-                    pid = fork();
-                    char **arg;
-                    if (pid == 0) {
-                        int arg_fd[3];
-                        int arg_ind = 0;
-                        
-                        /* handling file descriptors */
-                        for (int i = 0; i < 3; i++) {
-                            arg_fd[i] = atoi(argv[next_optind + i]);
-                        }
-                        
-                        /* handling arguments */
-                        int next_notFdOptind = next_optind + 3;
-                        for (; ((next_notFdOptind + arg_ind) != argc ) && !isOption(argv[next_notFdOptind + arg_ind]); arg_ind++) {
-                            arg[arg_ind] = argv[next_notFdOptind + arg_ind];
-                        }
-                        arg[arg_ind] = NULL;
-                        
-                        /* prepare standard streams */
-                        dup2(arg_fd[0], STDIN_FILENO);
-                        dup2(arg_fd[1], STDOUT_FILENO);
-                        dup2(arg_fd[2], STDERR_FILENO);
-                        
-                        if(verbose_flag){
-                            printf("--commmand");
-                            for (int i = 0; i < 3; i++) {
-                                printf(" %c", arg_fd[i]);
-                            }
-                            for (int j = 0; j < arg_ind; j++) {
-                                printf(" %s", arg[j]);
-                            }
-                        }
+					while((optind != argc) && !isAnOption(argv[optind])){
+						optind++;
+					}
+					if(optind < (curr_optind + 5))
+						fprintf(stderr, "error: missing arguments to --command, your command will not be run\n");
+					else{
+						
+						
+						cmd_fd[0] = atoi(optarg);
+						cmd_fd[1] = atoi(argv[next_optind++]);
+						cmd_fd[2] = atoi(argv[next_optind++]);
 
-                        if (execvp(arg[0], arg) == -1) {
-                            fprintf(stderr, "Error: cannot execute command \"%s\"\n", argv[0]);
-                            exit(EXIT_FAILURE);
-                        }
-                    }
-                    else if (pid < 0) {
-                        fprintf(stderr, "Error: cannot execute command \"%s\"\n", argv[0]);
-                    }
-                    else {
-                        int status, exstat;
-                        waitpid(pid, &status, 0);
-                        
-                        /* to get the lower 8-bit of the exit status */
-                        exstat = WEXITSTATUS(status);
-                    }
+						cmd_ind = 0;
+						//for(int i = 0; i < 3; i++)
+						//	printf("cmd_fd[%d] = %d\n", i, cmd_fd[i]);
+						
+						flushCmd();
+
+						while((next_optind != argc) && !isAnOption(argv[next_optind])){
+							char *char_ptr = malloc(sizeof(argv[next_optind]));
+							//printf("%d ", (int)sizeof(argv[next_optind]));
+							cmd[cmd_ind] = char_ptr;
+							strcpy(cmd[cmd_ind], argv[next_optind]);
+							cmd_ind++;
+							next_optind++;
+						}
+						if(verbose_flag){
+							printf("--command");
+							for(int i = 0; i < 3; i++){
+								printf(" %d", cmd_fd[i]);
+							}
+							for(int i = 0; i < cmd_size; i++){
+								if(cmd[i] != NULL){
+									printf(" %s", cmd[i]);
+								}
+							}
+							printf("\n");
+						}
+						
+						c_pid = fork();
+						if(c_pid == 0){	
+							
+							dup2(fd[cmd_fd[0]], STDIN_FILENO);
+							dup2(fd[cmd_fd[1]], STDOUT_FILENO);
+							dup2(fd[cmd_fd[2]], STDERR_FILENO);
+							if(execvp(cmd[0], cmd) == -1){
+								fprintf(stderr, "error: command failed");
+							}
+						}
+						else if(c_pid > 0){
+							int status;
+							waitpid(c_pid, &status, 0);
+							int exit_status = WEXITSTATUS(status);
+							if(wait_flag){
+								printf("%d", exit_status);
+								for(int i = 0; i < cmd_size; i++){
+									if(cmd[i] != NULL){
+										printf(" %s", cmd[i]);
+									}
+								}
+								printf("\n");
+							}
+							if(exit_status > return_val)
+								return_val = exit_status;
+						}
+						else{ //couldn't create child process
+							fprintf(stderr, "error: could not create child process\n");
+						}
+						
+						
+						for(int i = 0; i < cmd_size; i++)
+							free(cmd[i]);
+						
+					}
+					
 					break;
 				}
 				case '?':{
-					fprintf(stderr, "Error: option not recognized or no option argument found\n");
+					fprintf(stderr, "error: option not recognized or no option argument found\n");
 					break;
 				}
-                default:
-                    break;
 			}
 
 			//move to next option
-			while((optind != argc) && !isOption(argv[optind]))
+			while((optind != argc) && !isAnOption(argv[optind]))
 				optind++;
 			curr_optind = optind;
 			curr_opt = getopt_long(argc, argv, "", long_opts, &long_opts_ind);
@@ -171,9 +229,10 @@ int main(int argc, char **argv){
 	}
 
 	for(int i = 0; i < fd_ind; i++){
-		if(close(fd[i]) != 0)
-			fprintf(stderr, "Error: could not close file at logical index %d", i);
+		if(close(fd[i]) != 0){
+			fprintf(stderr, "error: could not close file at logical index %d\n", i);
+		}
 	}
 
-  return EXIT_SUCCESS;
+  return return_val;
 }
--- test.sh
+++ test.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 #put test cases here
-echo "./simpsh --rdonly a --wronly b --pipe --rdonly c"
-./simpsh --rdonly a --wronly b --pipe --rdonly c
+touch a b c d
+echo "./simpsh --verbose --rdonly a --wronly b --wronly c"
+./simpsh --verbose --rdonly a --wronly b --wronly c
 if [ $? -ne 0 ]; then
-	echo "Test 1 failed (temporary)"
-	exit
+	echo "Test 1 had errors "
 else
 	echo "Test 1 success"
+fi
+
+echo "./simpsh --verbose --rdonly a --wronly b --rdonly c"
+./simpsh --verbose --rdonly a --wronly b --rdonly c
+if [ $? -ne 0 ]; then
+	echo "Test 2 had errors "
+else
+	echo "Test 2 success"
+fi
+
+echo "./simpsh --verbose --rdonly a --wronly b --rdonly c --command 0 1 2 cat a -"
+./simpsh --verbose --rdonly a --wronly b --rdonly c --command 0 1 2 cat a -
+if [ $? -ne 0 ]; then
+	echo "Test 3 had errors "
+else
+	echo "Test 3 success"
 fi
